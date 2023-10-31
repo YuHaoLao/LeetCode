7f8c7b983b28d1d4eb4aa260c97d9fd5c1e409d1
yuhao Lao <97710831+YuHaoLao@users.noreply.github.com>
2023-10-31 00:30:04 -0400

    Time: 98 ms (5.10%) | Memory: 16.2 MB (90.08%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -1,8 +1,9 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
-        res=[words[0]] 
-        for i in range(1,len(words)):
-            mp1,mp2=Counter(words[i-1]),Counter(words[i]) 
-            if mp1!=mp2:
-                res.append(words[i]) 
+        res=[words[0]]
+        for i in range(1, len(words)):
+            word1, word2 = Counter(words[i-1]),Counter(words[i])
+            if word1 != word2:
+                res.append(words[i])
         return res
+
